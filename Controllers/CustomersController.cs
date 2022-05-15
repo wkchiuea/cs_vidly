b647fd4ac91c5f20d93f1f9a046a4c3a778fc695
cwklouis07 <49773448+cwklouis07@users.noreply.github.com>
2022-05-15 21:35:34 +0800

    initialize projects and add basic mvc items

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "John Smith", Id = 1 },
+                new Customer { Name = "Mary Williams", Id = 2 }
+            };
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        // Get: Customers/Details/{id}
+        public ActionResult Details(int id)
+        {
+            var customer = new List<Customer>
+            {
+                new Customer { Name = "John Smith" },
+                new Customer { Name = "Mary Williams" }
+            }[id-1];
+
+            return View(customer);
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Name = "Shrek" },
+                new Movie { Name = "Wall-e" }
+            };
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movies = movies
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")] // constraints
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+    }
+}
