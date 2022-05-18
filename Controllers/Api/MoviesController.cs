8dcbcbbb5a146acedcb3202727463059a290932c
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-05-18 22:33:56 +0800

    [update] use datatables in movies

--- Controllers/Api/MoviesController.cs
+++ Controllers/Api/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -19,7 +20,10 @@ namespace Vidly.Controllers.Api
 
         public IEnumerable<MovieDto> GetMovies()
         {
-            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+            return _context.Movies
+                .Include(m => m.Genre)
+                .ToList()
+                .Select(Mapper.Map<Movie, MovieDto>);
         }
 
         public IHttpActionResult GetMovie(int id)
